4439feeae7440f4d48c67b66dde935577c3c411b
gmlunesa <gmlunesa@gmail.com>
2021-12-02 01:28:08 +0800

    [Day 01] Added solution

---
+++ AdventOfCode2021/01SonarSweep/DepthCalculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Day01SonarSweep
+{
+    public class DepthCalculator
+    {
+        int[] _depthArray;
+        public DepthCalculator(string dataFile)
+        {
+            var depthValues = File.ReadAllLines(dataFile);
+            _depthArray = depthValues.Select(int.Parse).ToArray();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("First task: {0}", CalculateDepth(1));
+            Console.WriteLine("Second task: {0}", CalculateDepth(3));
+        }
+
+        private int CalculateDepth(int windowSize)
+        {
+            return _depthArray
+                    .Where((third, index) => index >= windowSize && third > _depthArray[index - windowSize])
+                    .Count();
+        }
+    }
+}
---
+++ AdventOfCode2021/01SonarSweep/Program.cs
@@ -0,0 +1,5 @@
+﻿using Day01SonarSweep;
+
+string depthDataFile = @"Data\DepthMeasurements.dat";
+DepthCalculator calculator = new DepthCalculator(depthDataFile);
+calculator.Run();
